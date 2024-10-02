1ac636c8feebeace941435e065b98314a81d0004
Hayk Karapetyan <karapetyan.hayk@gmail.com>
2024-10-03 00:33:56 +0100

    Convert command args into ReportsConfig

--- kidmon-reports/src/Main.cpp
+++ kidmon-reports/src/Main.cpp
@@ -1,5 +1,8 @@
 #include <core/utils/Log.h>
 #include <core/utils/StopWatch.h>
+
+#include <kidmon/common/Tracer.h>
+#include <kidmon/common/Utils.h>
 #include <kidmon/config/Config.h>
 #include <kidmon/repo/FileSystemRepository.h>
 
@@ -10,6 +13,43 @@ namespace {
 
 const fs::path g_reportsDir =
     "C:/Windows/System32/config/systemprofile/AppData/Local/kidmon/reports";
+
+struct ReportsConfig
+{
+    uint32_t minutes {0};
+    uint32_t hours {0};
+    uint32_t days {0};
+    uint32_t months {0};
+    std::vector<int> range;
+    std::vector<std::string> fields;
+    std::string title {};
+    std::string process {};
+    std::string username {};
+    bool caseSensitiveLookup {false};
+};
+
+template <typename T>
+void maybeGet(const std::string& name, const cxxopts::ParseResult& res, T& dest)
+{
+    if (res.count(name) > 0)
+    {
+        dest = res[name].as<T>();
+    }
+}
+
+void reportsConfigFromOpts(const cxxopts::ParseResult& res, ReportsConfig& conf)
+{
+    maybeGet("user",           res, conf.username);
+    maybeGet("minutes",        res, conf.minutes);
+    maybeGet("hours",          res, conf.hours);
+    maybeGet("days",           res, conf.days);
+    maybeGet("months",         res, conf.months);
+    maybeGet("range",          res, conf.range);
+    maybeGet("fields",         res, conf.fields);
+    maybeGet("title",          res, conf.title);
+    maybeGet("process",        res, conf.process);
+    maybeGet("case-sensitive", res, conf.caseSensitiveLookup);
+}
 
 TimePoint makeTimepoint(int year = 2024,
                         int month = 1,
@@ -30,24 +70,25 @@ TimePoint makeTimepoint(int year = 2024,
     return std::chrono::system_clock::from_time_t(std::mktime(&tm));
 }
 
-/// List users
-///      - users
-/// Query user
-///      - has title
-///      - has process
-///      - by date
-///      - by custom range
-///      - by month
-///      - from last M minutes
-///      - from last N hours
-/// Options
-///      - case sensitivity control
-///
+/**
+ * Creates TimePoint from the input expecting it to be an integer the format YYYYMMDD
+ */
+TimePoint makeTimepoint(uint32_t date)
+{
+    int day = date % 100;
+    date /= 100;
+    int month = date % 100;
+    date /= 100;
+    int year = date;
+
+    return makeTimepoint(year, month, day);
+}
 
 
 bool validateArguments(const cxxopts::ParseResult& result)
 {
     std::ignore = result;
+    // @todo:hayk - add validation logic
 
     return true;
 }
@@ -57,27 +98,43 @@ void handleListUsers()
     FileSystemRepository repo(g_reportsDir);
     spdlog::trace("Listing users...");
 
-    repo.queryUsers([](const std::string& username) {
-        std::cout << username << '\n';
+    std::ostringstream ss;
+    repo.queryUsers([&ss](const std::string& username) {
+        ss << '\n' << username;
         return true;
     });
 
+    spdlog::info("Users: {}", ss.str());
     spdlog::trace("Listing completed.");
 }
 
 
-void handleQueryUser(const StopWatch& sw, const std::string& username)
+void handleQueryUser(const ReportsConfig& conf)
 {
     FileSystemRepository repo(g_reportsDir);
+    TimePoint to = SystemClock::now();
+    TimePoint from = to;
 
-    // @todo:hayk - extend input argument and init filter from input
-    const TimePoint from = makeTimepoint(2024, 1, 22, 0, 0, 0);
-    const TimePoint to = makeTimepoint(2024, 9, 25, 0, 0, 0);
-    Filter filter(username, from, to);
+    if (conf.range.empty())
+    {
+        from -= std::chrono::minutes(conf.minutes);
+        from -= std::chrono::hours(conf.hours);
+        from -= std::chrono::days(conf.days);
+        from -= std::chrono::months(conf.months);
+    }
+    else
+    {
+        from = (conf.range.size() > 0) ? makeTimepoint(conf.range[0]) : TimePoint::min();
+        to = (conf.range.size() > 1) ? makeTimepoint(conf.range[1]) : SystemClock::now();
+    }
 
-    int64_t prevUpdate {};
+    Filter filter(conf.username, from, to);
+
+    StopWatch sw(true);
+    int64_t prevUpdate {100};   // skip logging the status for short queries
     int numEntries = 0;
-    repo.queryEntries(filter, [&sw, &numEntries, &prevUpdate](const Entry& entry) {
+
+    repo.queryEntries(filter, [&sw, &numEntries, &prevUpdate, &conf](const Entry& entry) {
         if (sw.elapsedMs() - prevUpdate > 100)
         {
             prevUpdate = sw.elapsedMs();
@@ -95,14 +152,11 @@ void handleQueryUser(const StopWatch& sw, const std::string& username)
 
 int main(int argc, char* argv[])
 {
+    std::optional<ScopedTrace> trace;
+
     try
     {
-        AppConfig conf;
-        conf.logFilename = "kidmon-reports.log";
-        utl::configureLogger(conf.logsDir, conf.logFilename);
-
-        cxxopts::Options opts("kidmon-reports",
-                              "Produce reports for the user activity");
+        cxxopts::Options opts("kidmon-reports", "Produce reports for the user activity");
 
         // clang-format off
         opts.add_options()
@@ -121,6 +175,14 @@ int main(int argc, char* argv[])
         ;
         // clang-format on
 
+        AppConfig conf;
+        conf.logFilename = "kidmon-reports.log";
+        utl::configureLogger(conf.logsDir, conf.logFilename);
+
+        trace.emplace("",
+                      fmt::format("{:-^80s}", "> START <"),
+                      fmt::format("{:-^80s}\n", "> END <"));
+
         const auto result = opts.parse(argc, argv);
 
         if (result.count("help") || !validateArguments(result))
@@ -128,7 +190,6 @@ int main(int argc, char* argv[])
             std::cout << opts.help() << std::endl;
             return 2;
         }
-
 
         StopWatch sw;
         sw.start();
@@ -139,10 +200,12 @@ int main(int argc, char* argv[])
         }
         else
         {
-            handleQueryUser(sw, result["user"].as<std::string>());
+            ReportsConfig reportsConf;
+            reportsConfigFromOpts(result, reportsConf);
+            handleQueryUser(reportsConf);
         }
 
-        spdlog::info("Processing took: {}ms", sw.elapsedMs());
+        spdlog::info("Processing took: {}ms", utl::humanizeDuration(sw.elapsed()));
     }
     catch (const std::exception& e)
     {
