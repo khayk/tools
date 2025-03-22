4a0ac6e94a8878c7f7622b349170c9114d1c8542
Hayk Karapetyan <karapetyan.hayk@gmail.com>
2025-03-22 11:05:24 +0000

    Update tests

--- test/core/utils/FileTest.cpp
+++ test/core/utils/FileTest.cpp
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 #include <core/utils/File.h>
 
+#include <exception>
 #include <filesystem>
 
 namespace fs = std::filesystem;
@@ -48,25 +49,23 @@ TEST_F(UtilsFileTests, ReadLines)
     write(file, "one\ntwo\n\nfour");
 
     std::vector<std::string> lines;
-    std::error_code ec;
 
-    EXPECT_TRUE(readLines(file, [&lines](const std::string& line) {
+    EXPECT_NO_THROW(readLines(file, [&lines](const std::string& line) {
         lines.push_back(line);
         return true;
-    }, ec));
+    }));
 
-    EXPECT_TRUE(!ec);
-    EXPECT_EQ(4,      lines.size());
+    ASSERT_EQ(4,      lines.size());
     EXPECT_EQ("one",  lines[0]);
     EXPECT_EQ("two",  lines[1]);
     EXPECT_EQ("",     lines[2]);
     EXPECT_EQ("four", lines[3]);
 
     lines.clear();
-    EXPECT_FALSE(readLines(file, [&lines](const std::string& line) {
+    EXPECT_NO_THROW(readLines(file, [&lines](const std::string& line) {
         lines.push_back(line);
         return false;
-    }, ec));
+    }));
     // We have instructed to stop enumeration after the first entry
     EXPECT_EQ(1,      lines.size());
 }
@@ -74,14 +73,12 @@ TEST_F(UtilsFileTests, ReadLines)
 TEST_F(UtilsFileTests, ReadLinesFails)
 {
     const fs::path file = testDir_ / "data.txt";
-    std::error_code ec;
 
     MockFunction<void(const std::string&)> mock;
     EXPECT_CALL(mock, Call(_)).Times(0);
 
     auto lambda = [&mock](const std::string& line) { mock.Call(line); return true; };
-    EXPECT_FALSE(readLines(file, lambda, ec));
-    EXPECT_FALSE(!ec);
+    EXPECT_THROW(readLines(file, lambda), std::exception);
 }
 
 } // namespace
