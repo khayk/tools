acc27325e7a5bf7ae172e3bafac834326451f404
Hayk Karapetyan <karapetyan.hayk@gmail.com>
2025-05-11 18:39:07 +0100

    Update in clang tidy cmake files

--- cmake/ClangTidy.cmake
+++ cmake/ClangTidy.cmake
@@ -1,13 +1,15 @@
 function(AddClangTidy target)
 	if(TOOLS_TIDY)
-		find_program(CLANG-TIDY_PATH clang-tidy REQUIRED)
-		message("Found clang-tidy      : ${CLANG-TIDY_PATH}")
+		find_program(CLANG_TIDY_PATH NAMES "clang-tidy-19" REQUIRED)
+		message("Found clang-tidy      : ${CLANG_TIDY_PATH}")
 
 		set_target_properties(
 			${target}
 			PROPERTIES CXX_CLANG_TIDY
-			"${CLANG-TIDY_PATH};--extra-arg=/EHsc"
-			#CXX_CLANG_TIDY"${CLANG-TIDY_PATH};--extra-arg=/EHsc;-checks=*;--warnings-as-errors=*"
+			"${CLANG_TIDY_PATH}"
+			# "${CLANG_TIDY_PATH};-checks=*;--warnings-as-errors=*"
+			# "${CLANG_TIDY_PATH};--extra-arg=/EHsc"
+			#CXX_CLANG_TIDY"${CLANG_TIDY_PATH};--extra-arg=/EHsc;-checks=*;--warnings-as-errors=*"
 		)
 	endif()
 endfunction()
