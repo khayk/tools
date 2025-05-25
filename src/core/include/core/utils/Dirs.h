fad5d981ee1d6a5b4f936afb33aebbb3b66a6c01
Hayk Karapetyan <karapetyan.hayk@gmail.com>
2025-05-25 16:18:32 +0100

    Add function to get cache directory

--- src/core/include/core/utils/Dirs.h
+++ src/core/include/core/utils/Dirs.h
@@ -16,17 +16,27 @@ namespace dirs {
 fs::path home(std::error_code& ec);
 fs::path home();
 
+
 /**
  * @brief Temp directory of the current account. On Windows systems this is %TEMP%
  */
 fs::path temp(std::error_code& ec);
 fs::path temp();
 
+
 /**
  * @brief Application data directory. On Windows systems, this is '%LOCALAPPDATA%'.
  */
 fs::path data(std::error_code& ec);
 fs::path data();
+
+
+/**
+ * @brief Application cache directory.
+ */
+fs::path cache(std::error_code& ec);
+fs::path cache();
+
 
 /**
  * @brief Returns the systemwide config directory. On Windows systems, this is
