acfd57dfc2951bb0d4cf5e37e13396198eb85c65
Hayk Karapetyan <karapetyan.hayk@gmail.com>
2026-05-28 20:50:57 +0100

    Fxi token generation is predictablility issue

--- kidmon/src/common/Utils.cpp
+++ kidmon/src/common/Utils.cpp
@@ -9,11 +9,18 @@ namespace km::utl {
 
 std::string generateToken(const size_t length)
 {
+    // This initialization happens ONLY on the very first function call
+    static const bool isSeeded = []() {
+        std::srand(static_cast<unsigned int>(std::time(nullptr)));
+        return true;
+    }();
+    std::ignore = isSeeded;
+
     const auto randomChar = []() -> char {
         constexpr std::string_view charset = "0123456789"
                                              "abcdefghijklmnopqrstuvwxyz"
                                              "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        return charset[static_cast<unsigned long>(rand()) % (charset.size() - 1)];
+        return charset[static_cast<unsigned long>(rand()) % charset.size()];
     };
 
     std::string token(length, 0);
